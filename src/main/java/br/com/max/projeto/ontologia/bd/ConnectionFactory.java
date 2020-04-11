46427d740d5255089fe21b4a0f2a09a928949bb8
Max Denner <maxdenner027@hotmail.com>
2020-04-11 10:03:15 -0300

    Alterações na conexão com o bd

--- src/main/java/br/com/max/projeto/ontologia/bd/ConnectionFactory.java
+++ src/main/java/br/com/max/projeto/ontologia/bd/ConnectionFactory.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class ConnectionFactory {
 	public Connection getConnection() {
-		String url = "ec2-34-193-232-231.compute-1.amazonaws.com";
+		String url ="postgres://iqotyfuolprwdb:f17de2e6db5a5f7cde274ca9f56fbd4a26ad615afdb07605863c61d18e408848@ec2-34-193-232-231.compute-1.amazonaws.com:5432/d26h4e9k8an93o";
 		String user = "iqotyfuolprwdb";
 		String pass = "f17de2e6db5a5f7cde274ca9f56fbd4a26ad615afdb07605863c61d18e408848";
 		try {
